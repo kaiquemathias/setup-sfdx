a8db9d89de3ad743bf7efce6963e448ea13955cc
kaiquemathias <38389391+kaiquemathias@users.noreply.github.com>
2021-01-14 15:01:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ try {
 }
 
 function installSFDX(){
-  var download = 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz -P /tmp'
+  var download = 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz'
   var createDir = 'mkdir sfdx'
-  var unzip = 'tar xJf /tmp/sfdx-linux-amd64.tar.xz -C sfdx --strip-components 1'
+  var unzip = 'tar xJf sfdx-linux-amd64.tar.xz -C sfdx --strip-components 1'
   var install = './sfdx/install'
-  var clean = 'rm -r ./sfdx'
-  exec(download+' && '+createDir+' && '+unzip+' && '+install+' && '+clean, function(error, stdout, stderr){
+  exec(download+' && '+createDir+' && '+unzip+' && '+install, function(error, stdout, stderr){
     if(error) throw(stderr)
     core.debug(stdout)
     if(core.getInput('sfdx-auth-url')) createAuthFile()
@@ -22,15 +21,14 @@ function installSFDX(){
 }
 
 function createAuthFile(){
-  fs.writeFileSync('/tmp/sfdx_auth.txt', core.getInput('sfdx-auth-url'))
+  fs.writeFileSync('./sfdx_auth.txt', core.getInput('sfdx-auth-url'))
   authSFDX()
 }
 
 function authSFDX(){
   var params = '--setdefaultdevhubusername --setdefaultusername -a SFDX-ENV'
-  exec('sfdx auth:sfdxurl:store -f /tmp/sfdx_auth.txt '+params, function(error, stdout, stderr){
+  exec('sfdx force:auth:sfdxurl:store -f ./sfdx_auth.txt '+params, function(error, stdout, stderr){
     if(error) throw(stderr)
 	core.debug(stdout)
   })
 }
-
